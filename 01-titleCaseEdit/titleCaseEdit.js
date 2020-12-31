09ff6e7b11b52a435adfa4f093506c01cf228587
bw0123 <72894015+bw0123@users.noreply.github.com>
2020-12-31 14:12:35 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let capitalLetters = title.split(" ");
+  
+  for (let i = 0; i < capitalLetters.length; i++){
+    capitalLetters[i] = capitalLetters[i].charAt(0).toUpperCase + capitalLetters[i].substr(1);
+  }
+  
+  return capitalLetters.join(' ');
 }
 
 // Do not edit this line;
