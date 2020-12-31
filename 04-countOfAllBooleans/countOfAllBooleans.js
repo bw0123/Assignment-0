612120f1838d4e0595435076c07ca2ca2fc39037
bw0123 <72894015+bw0123@users.noreply.github.com>
2020-12-31 15:04:41 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -2,10 +2,10 @@ function countOfAllBooleans(arr) {
   // Insert code here;
   var count = 0;
   for(var i = 0; i < arr.length; i++){
-    if (arr[i] == true){
+    if (arr[i] === true){
       count += 1;
     }
-    if (arr[i] == false){
+    if (arr[i] === false){
       count += 1;
     }
   }
