271d88867f6c35f2ac33af1e785b535f25593c9a
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-01 13:50:16 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var tally =  0;
+  for (var i = 0; i < nums.length; i++){
+    if (num[i] < target){
+      tally+= 1;
+    }
+  }
+  return tally;
 }
 
 // Do not edit this line;
