9cad530bd4846b5a34b0e0e6eaaa43cdcdc43897
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-01 14:22:27 -0500

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var count = 0;
+  for (var i = 0; i < nums.length; i++){
+  // If the number in the array is odd, increase the count
+    if ((nums[i] % 2) != 0){
+     count+= 1;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
