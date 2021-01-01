b7d636d598a2605cf607619eaae80d15be328b93
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-01 14:17:30 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var count = 0;
+  if (var i = 0; i < nums.length; i++){
+    // If the number in the array is even, increase the count
+    if (nums[i] % 2 == 0){
+      count+= 1;  
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
