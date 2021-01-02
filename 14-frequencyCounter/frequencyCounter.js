5465149b362320575509d2e0d116951be34a50e2
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-02 15:20:07 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,19 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var frequency = 1;
+  var count = 1;
+  
+  for(var i = 0; i < word.length; i++){
+  // Increase count if letters match
+    if((word.charAt(i)) == word.charAt(i + 1)){
+      frequency+= 1;
+      document.write(" ", word.charAt(i), " : ", frequency, " ");
+    }
+    else{
+      count == 1;
+      document.write(" ", word.charAt(i), " : ", count, " ");
+    }
+  }
 }
 
 // Do not edit this line;
