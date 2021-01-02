6c8a6d0b1f2530774afa279ad9dd299918dd1c37
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-02 15:29:51 -0500

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,12 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    var sum = 0;
+    // Add numbers from descending order, set i = num instead of 0
+    for (var i = num; i >= 0; i--){
+     sum+= i; 
+    }
+    document.write(sum);
   }
 }
 
