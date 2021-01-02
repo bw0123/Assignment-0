2931427c410a24686b6bd1fbe6a127a5aead551c
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-02 14:12:17 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
+  var reverse = ' ';
+  
+  for(var j = word.length - 1; i >= 0; i--){
+    reverse+= word[i];
+  }
+  if(word === reverse){
+    document.write(Boolean(true));
+  }
+  else if(word != reverse){
+    document.write(Boolean(false));
+  }
 }
 
 // Do not edit this line;
