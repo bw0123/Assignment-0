ec5d1d1e18787e079d8a838c375b3b4b81ce4332
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-03 14:56:28 -0500

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,24 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let first = 0;
+    let last = nums.length - 1;
+    
+    // As we read the entire array
+    while(first <= last){
+      let midpoint = Math.floor((first + last) / 2); 
+      
+      if (target > nums[midpoint]){
+        first = midpoint + 1;
+      }
+      else if (target < nums[midpoint]){
+        last = midpoint - 1; 
+      }
+      else (target === nums[midpoint]){           
+        return true;                                  // The value exists in the array
+      }
+    }
+    return false;                                     // The value DNE in the array
   }
 }
 
