dc6f0726fe4e96f57f9f9445fee344e1d51f8be5
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-03 14:49:44 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,23 @@
 function pairSum(nums, target) {
   // Insert code here;
+  var sum = 0;
+  let nums = [];
+  
+  if (nums.length <= 1){
+    throw "Array length needs to be larger than 1";
+  } 
+  
+  for (var i = 0; i < nums.length; i++){
+    for (var j = i+1; j < nums.length; j++){
+     if (target === nums[i]){
+      return true; 
+     }
+     else if ((nums[i] + nums[j]) === target && i != j){
+        return true;
+     }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
