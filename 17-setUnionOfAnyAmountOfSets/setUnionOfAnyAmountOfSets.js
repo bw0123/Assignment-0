eec6eca8eaf8e2582ec4f8556572e256319d6288
bw0123 <72894015+bw0123@users.noreply.github.com>
2021-01-03 14:41:53 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let obj = new Set();
+  
+  for (var i = 0; i < args.length, i++){
+    for(let numbers of args[i]){
+     finalSet.add(numbers); 
+    }
+  }
+  return obj;
 }
 
 // Do not edit this line;
